68ad43cb5be06d591bd667cb1ed0b46584267d70
njorogedavid13@gmail.com <njorogedavid13@gmail.com>
2017-09-01 10:18:56 +0300

    fix the local storage from page 3 and 1 to 2

--- js/scripts2.js
+++ js/scripts2.js
@@ -1,4 +1,36 @@
 genre = []
+actionCipher = []
+indieCipher = []
+casualCipher = []
+adventureCipher = []
+sportsCipher = []
+racingCipher = []
+
+
+var genreCipher = function() {
+  for (var i = 0; i < games.length; i++) {
+    if (games[i].genre === "Action") {
+      actionCipher.push(games[i])
+    } else if (games[i].genre === "Indie Games") {
+      indieCipher.push(games[i])
+    } else if (games[i].genre === "Racing") {
+      racingCipher.push(games[i])
+
+    } else if (games[i].genre === "Sports") {
+      sportsCipher.push(games[i])
+
+    } else if (games[i].genre === "Adventure") {
+      adventureCipher.push(games[i])
+
+    } else if (games[i].genre === "Casual") {
+      casualCipher.push(games[i])
+
+    }
+  }
+}
+
+
+
 var screenShots = function() {
   for (var i = 0; i < localObj[page1].screenS.length; i++) {
     // localObj[page1].screenS.[i]
@@ -11,7 +43,6 @@ var others = function() {
     if (localObj[i].genre === localObj[page1].genre) {
       //   $('#carddeck').append('<div class="card qwerty" id="' + i + '"><a href="page2.html">' + localObj[i].name + '</a><img class="card-img-top" id="' + i + '" src="' + localObj[i].img + '" width="243" alt="Card image cap"></div>')
       genre.push(localObj[i])
-      console.log(genre);
     }
   }
   for (var i = 0; i < 3; i++) {
@@ -27,8 +58,10 @@ var others = function() {
 jQuery(document).ready(function($) {
   localObj = JSON.parse(localStorage.getItem('data'))
   page1 = JSON.parse(localStorage.getItem('id'))
-  console.log(genre);
-  console.log(localObj[page1]);
+  console.log(localObj);
+  games = localObj
+  genreCipher()
+  console.log(indieCipher);
   $("#gamesHeaders").append(localObj[page1].name) //changes  game headers for page 2
   $("#intro,#description").css('background-image', "url('" + localObj[page1].img + "')") //changes background for section page 2
   $(".sc").append('<img src="' + localObj[page1].img + '" id="thumb" alt="">') //changes page 2 thumbnail
